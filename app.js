7cae294ed49c824cb9391b487320960166d1936b
Ayu Sudi <ayusudi.abc@gmail.com>
2022-11-08 09:25:10 +0700

    Fix routes name

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const express = require("express")
 const app = express()
 const port = 3000 
 
-app.get("/", (req, res) => {
+app.get("/pokemons", (req, res) => {
+  console.log("HAI");
+  res.status(200).json([{
+    id: 1, 
+    name: "Pikachoi"
+  }])
+})
+
+app.get("/pokemons/:id", (req, res) => {
   console.log("HAI");
   res.status(200).json([{
     id: 1, 

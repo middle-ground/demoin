2bba6ee3b3c722be28cf26038e9e0614549900be
Ayu Sudi <ayusudi.abc@gmail.com>
2022-11-08 09:46:03 +0700

    Create conflict

--- app.js
+++ app.js
@@ -3,23 +3,15 @@ const app = express()
 const port = 3000 
 
 app.get("/pokemons", (req, res) => {
-  console.log("HAI");
+  console.log("HALLOOOO");
   res.status(200).json([{
     id: 1, 
     name: "Pikachoi"
   }])
 })
 
-app.put("/pokemons/:id", (req, res) => {
-  res.status(200).json({message: "Update pokemon PUT"})
-})
-
-app.patch("/pokemons/:id", (req, res) => {
-  res.status(200).json({message: "Update pokemon PATCH"})
-})
-
 app.get("/pokemons/:id", (req, res) => {
-  console.log("HAI");
+  console.log("HIAAA");
   res.status(200).json([{
     id: 1, 
     name: "Pikachoi"
@@ -31,5 +23,5 @@ app.post("/pokemons", (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log("Aplikasi berjalan");
+  console.log("Aplikasi berjalan 3000");
 })
